e89918ecf096d0eb1419e780069f7a76087eece0
Tom C. <tom.carlier@epitech.eu>
2024-01-28 16:43:21 +0100

    [+] Init the repo

---
+++ include/my.h
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <stdarg.h>
+    #include <errno.h>
+    #include <math.h>
+    #include <stdlib.h>
+    #include <signal.h>
+
+    #include <criterion/criterion.h>
+    #include <criterion/redirect.h>
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Config.h>
+    #include <SFML/GPUPreference.h>
+    #include <SFML/Network.h>
+    #include <SFML/OpenGL.h>
+    #include <SFML/System.h>
+    #include <SFML/Window.h>
+
+#endif
