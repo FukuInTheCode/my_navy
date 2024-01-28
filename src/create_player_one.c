9618fefbad9a8ee08f7e372041d49e23c7333818
Tom C. <tom.carlier@epitech.eu>
2024-01-28 16:43:26 +0100

    [+] Add the file path error handling for the player 1

--- src/create_player_one.c
+++ src/create_player_one.c
@@ -7,3 +7,17 @@
 
 #include "my.h"
 
+static int handle_arg(FILE *fd)
+{
+    if (!fd)
+        return 84;
+
+    return 0;
+}
+
+int create_creating_player(char *file_path)
+{
+    FILE *fd = fopen(file_path, "r");
+
+    return 0;
+}
