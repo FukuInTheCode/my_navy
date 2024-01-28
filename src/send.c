72534306a18a04cfc2952dbfe315fda7921d6b1e
Tom C. <tom.carlier@epitech.eu>
2024-01-28 16:44:21 +0100

    [~] Change the usleep time

--- src/send.c
+++ src/send.c
@@ -12,11 +12,11 @@ int send_resp(gid_t pid, int response)
 {
     uint8_t bit = 0;
 
-    usleep(1e6);
+    usleep(1e4);
     for (int i = 0; i < 32; i++) {
         bit = response & (1 << i);
         kill(pid, bit != 0 ? SIGUSR2 : SIGUSR1);
-        usleep(10000);
+        usleep(1000);
     }
     return 0;
 }
