94ac3d0e257d63d290dcf2cdb80e97bb63928966
Tom C. <tom.carlier@epitech.eu>
2024-01-28 16:43:57 +0100

    [~] add the formating

--- src/game_loop.c
+++ src/game_loop.c
@@ -27,20 +27,32 @@ static int print_boards(char *player_map, char *enemy_map)
     return 0;
 }
 
-static int get_usr_move(player_t *player, char *enemy_map)
+static int get_usr_move(player_t *player, char *enemy_map, uint32_t turn_count)
 {
     char resp_buf[1001] = {0};
 
     read(0, resp_buf, 1000);
-    printf("%s\n", resp_buf);
+    write(1, "result: ", 8);
+    if (my_strlen(resp_buf) != 2)
+        return 84;
+    resp_buf[2] = ':';
+    write(1, resp_buf, 3);
+    write(1, "\n\n", 2);
+    turn_count++;
+    !(turn_count % 2) && print_boards(player->player_map, enemy_map);
     return 0;
 }
 
-int get_enemy_move(player_t *player, char *enemy_map)
+int get_enemy_move(player_t *player, char *enemy_map, uint32_t turn_count)
 {
+    write(1, "waiting for enemy's attack...\n\n", 31);
     player->bit_count = 0;
     for (player->response = 0; player->bit_count < 8;);
-    printf("%d\n", player->response);
+    write(1, "result: ", 8);
+    my_put_nbr(player->response);
+    write(1, "\n\n", 2);
+    turn_count++;
+    !(turn_count % 2) && print_boards(player->player_map, enemy_map);
     return 0;
 }
 
@@ -59,6 +71,7 @@ int game_loop(player_t *player)
 {
     int error = print_pid(player->pid);
     char *enemy_map = my_strdup(map_template);
+    uint32_t turn_count = 0;
 
     if (!enemy_map)
         return 84;
@@ -66,11 +79,11 @@ int game_loop(player_t *player)
     handle_player_one(player, enemy_map);
     for (; player->wstatus == WAITING_PLAYER1;);
     if (player->wstatus == WAITING_USER)
-        error |= get_usr_move(player, enemy_map);
+        error |= get_usr_move(player, enemy_map, turn_count);
     if (player->wstatus == WAITING_MOVE) {
         write(1, "successfully connected to enemy\n\n", 33);
         print_boards(player->player_map, enemy_map);
-        error |= get_enemy_move(player, enemy_map);
+        error |= get_enemy_move(player, enemy_map, turn_count);
     }
     free(enemy_map);
     return error;
